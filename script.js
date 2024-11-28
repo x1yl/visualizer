16ee8c6e3e543ad4d43f2e9e3610816e4f0766b4
x1yl <life23243@tutanota.com>
2024-11-28 17:59:23 -0500

    add choice of coins displayed

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("test").addEventListener("submit", function (event) {
+  document.getElementById("form").addEventListener("submit", function (event) {
     event.preventDefault();
-    const input = document.querySelector("input");
-    const value = input.value;
+    const value = document.querySelector("input").value;
+    const cents = value * 100;
     const coinDiv = document.getElementById("coins");
+    const coinType = document.getElementById("coinType").value;
+
+    const coinValues = {
+      quarter: 25,
+      dime: 10,
+      nickel: 5,
+      penny: 1,
+    };
+
+    const numberOfCoins = cents / coinValues[coinType];
+
+    coinDiv.innerHTML = "";
+    document.getElementById("coinType")
     console.log(value);
-    for (let i = 0; i < value; i++) {
+    for (let i = 0; i < numberOfCoins; i++) {
       const img = document.createElement("img");
-      img.src = "penny.png"
-      img.style.width = "50px";
-      img.style.height = "50px";
+      img.src = `${coinType}.png`;
+      img.style.width = "75px";
+      img.style.height = "75px";
       img.style.margin = "5px";
       coinDiv.appendChild(img);
     }
